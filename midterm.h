04f5c2beb1db58617aa566bba305752989dc4503
U160709034 <guraycoskun@posta.mu.edu.tr>
2020-05-03 18:26:23 +0300

    updated

--- midterm.h
+++ midterm.h
@@ -1,15 +1,34 @@
-#ifndef midterm
-#define midterm
-struct s {
+#ifndef denemeler
+#define denemeler
+int first;
+
+typedef enum{
+	SQ,
+	PR,
+	DF,
+	PF,
+	AB
+}type;
+typedef struct {
+	type t;
+
 	int boyut;
 	int tepe;
 	int * dizi;
+ 	char type[3];
+	int nrOfOpr;
+	int number;
+} s;
+struct node {
+ s data;
+ struct node *next;
 };
-int num;
+int sqr;
 int x;
 typedef s stack;
 stack * tanim();
 int pop(stack *);
+
 void push(int,stack *);
 void bastir(stack*);
 int abudant(int );
